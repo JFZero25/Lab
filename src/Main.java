ce8eac731e86af19e08c9197f3bb6f3afd742a93
JF <j.roman04@ufromail.cl>
2025-04-10 10:54:03 -0400

    se realizaron los ejercios 2 y 3 (Almost)

--- src/Main.java
+++ src/Main.java
@@ -3,64 +3,89 @@ import java.util.Scanner;
 
 public class Main {
     private static Scanner scanner = new Scanner(System.in);
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         menu();
     }
 
-    public static void menu(){
+    public static void menu() {
         int seleccion;
         Crearmatriz();
         do {
             seleccion = scanner.nextInt();
             Mostraropciones();
             Ejecutar(seleccion);
-        }while (seleccion != 0) ;
+        } while (seleccion != 0);
     }
 
-private static void Mostraropciones(){
+    private static void Mostraropciones() {
         System.out.println("    Bienvenido   ");
         System.out.println("    1-Crear matriz   ");
         System.out.println("    2-Mostrar fila   ");
         System.out.println("    0-Salir   ");
-}
-
-private static void Ejecutar(int seleccion){
-        if (seleccion==1){
-            Crearmatriz();
-        }
-        if (seleccion==2){
-            Mostrarmatriz(int matriz[][]);
-        }
-}
-
-private static void Crearmatriz(){
-    System.out.println("Cuantas filas");
-    int ext;
-    int filas=scanner.nextInt();
-    Validar1(filas);
-    System.out.println("cuantas columnas");
-    int columnas=scanner.nextInt();
-    Validar2(columnas);
-    int [][] matriz=new int[filas][columnas];
     }
 
-private static int Validar1(int filas){
-        if (filas>0){
-            if (filas<9) {
+    private static void Ejecutar(int seleccion) {
+        if (seleccion == 1) {
+            Crearmatriz();
+        }
+        if (seleccion == 2) {
+            int matriz[][];
+            int fila = scanner.nextInt();
+            Mostrarmatriz(matriz[][],fila);
+        }
+    }
+
+    private static void Crearmatriz() {
+        System.out.println("Cuantas filas");
+        int filas = scanner.nextInt();
+        Validar1(filas);
+        System.out.println("cuantas columnas");
+        int columnas = scanner.nextInt();
+        int[][] matriz = new int[filas][columnas];
+        Llenarmatriz(matriz);
+    }
+
+    private static int Validar1(int filas) {
+        if (filas > 0) {
+            if (filas < 9) {
                 return (filas);
-            } else{
+            } else {
                 System.out.println("Error de rango (<9)");
             }
-        }else {System.out.println("Error de input");}
+        } else {
+            System.out.println("Error de input");
+        }
+    }
 
-private static void Validar2(int colimnas){
+    private static void Llenarmatriz(int[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = (int) (Math.random() * 10);
+            }
+        }
+    }
 
-}
+    private static void Mostrarmatriz(int matriz[][], int fila) {
+        for (int j = 0; j < matriz[fila].length; j++) {
+            System.out.println(matriz[fila][j]);
+        }
+    }
 
-private static void Llenarmatriz(int[][]matriz){
-
-}
-private static void Mostrarmatriz(int matriz[][]){
-
-}
+    private static void Matrizcero(int matriz[][]) {
+        int contador = 0;
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                int suma = i + j;
+                if (matriz[i][j] == 0) {
+                    contador++;
+                    if (contador / suma == 1 / 2) {
+                        System.out.println("Es una matriz cero");
+                    } else {
+                        System.out.println("No es una matriz cero");
+                    }
+                }
+            }
+        }
+    }
 }
