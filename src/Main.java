6111fc0d0a3c10cf5f1388776592876090bb09ec
JF <j.roman04@ufromail.cl>
2025-04-10 09:44:53 -0400

    Se realizo la actividad 1 y 2 y se empezo las validaciones y la act 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,66 @@
+import javax.swing.plaf.TreeUI;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    private static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args){
+        menu();
     }
+
+    public static void menu(){
+        int seleccion;
+        Crearmatriz();
+        do {
+            seleccion = scanner.nextInt();
+            Mostraropciones();
+            Ejecutar(seleccion);
+        }while (seleccion != 0) ;
+    }
+
+private static void Mostraropciones(){
+        System.out.println("    Bienvenido   ");
+        System.out.println("    1-Crear matriz   ");
+        System.out.println("    2-Mostrar fila   ");
+        System.out.println("    0-Salir   ");
+}
+
+private static void Ejecutar(int seleccion){
+        if (seleccion==1){
+            Crearmatriz();
+        }
+        if (seleccion==2){
+            Mostrarmatriz(int matriz[][]);
+        }
+}
+
+private static void Crearmatriz(){
+    System.out.println("Cuantas filas");
+    int ext;
+    int filas=scanner.nextInt();
+    Validar1(filas);
+    System.out.println("cuantas columnas");
+    int columnas=scanner.nextInt();
+    Validar2(columnas);
+    int [][] matriz=new int[filas][columnas];
+    }
+
+private static int Validar1(int filas){
+        if (filas>0){
+            if (filas<9) {
+                return (filas);
+            } else{
+                System.out.println("Error de rango (<9)");
+            }
+        }else {System.out.println("Error de input");}
+
+private static void Validar2(int colimnas){
+
+}
+
+private static void Llenarmatriz(int[][]matriz){
+
+}
+private static void Mostrarmatriz(int matriz[][]){
+
+}
 }
